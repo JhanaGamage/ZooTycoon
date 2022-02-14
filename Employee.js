9b2fbb5cb523e3320f3854aba11887df11626a2d
JametHugo <hugojamet2014@gmail.com>
2022-02-14 16:28:08 +0100

    add

--- Employee.js
+++ Employee.js
@@ -1,7 +1,17 @@
 import LivingBeing from "./LivingBeing";
 
 class Employee extends LivingBeing{
-    constructor ()
+    constructor (available = true, exp = 0){
+    this.available = available;
+    this.exp = exp
+    }
+    Infarction() {
+        let x = Math.random(); 
+        if (x = 10) {
+            pv = pv - 10
+            alert ("Cette employé a perdu 10 pv.")
+        }
+    }
 }
 
 export default Employee;
